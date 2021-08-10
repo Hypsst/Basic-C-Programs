c2cb7bada279e2b21346a7386b33763790eb7edd
Hyps <hypsster@gmail.com>
2021-08-10 14:39:38 -0400

    Initial commit

---
+++ HpCountDown.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    int hp = 100;
+
+    while (true)
+    {
+        hp -= 1;
+        cout << hp << " \n";
+        Sleep(3000);
+    }
+
+}
